10fa30e20ce530f19bb33d52e7db4c67d6dea219
Gian wong <139979396+Vyscan@users.noreply.github.com>
2025-02-27 10:46:14 +0800

    Final (completed version using mysql installer and workbench)
    Its completed but some of the designs are not good

---
+++ ITS120L-main/ITS120L-main/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tummy Pillow</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+    <?php include 'header.php'; ?>
+	</header>
+        <section class="welcome-message">
+            <h2>Welcome to Tummy Pillow, where there's comfort in every bite!</h2>
+        </section>
+        <section class="buttons-container">
+            <a href="menu.php" class="circle-button">(Photo)<br>Menu</a>
+            <a href="login.php" class="circle-button">(Photo)<br>Login</a>
+        </section>
+    </div>
+</body>
+</html>
